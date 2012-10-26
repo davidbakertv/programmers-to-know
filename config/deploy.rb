bd550a6e2e9e9baa7b37e3a4b3b6c653abe46d02
David Baker <2012@davidbaker.tv>
2012-10-26 11:12:58 -0400

    capistrano settings

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "nerdio"
+set :repository,  "git@github.com:davidbakertv/programmers-to-know.git"
+set :user, "akivadavid2"
+set :deploy_to, "/home/#{ user }/#{ application }"
+set :use_sudo, false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+default_run_options[:pty] = true
+
+role :web, "96.8.123.78"                          # Your HTTP server, Apache/etc
+role :app, "96.8.123.78"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
